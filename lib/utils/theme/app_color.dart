a9bacb4def49731f48e09f61e68cc5ad0311631e
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-24 20:13:08 +0700

    feat: add new color

--- lib/utils/theme/app_color.dart
+++ lib/utils/theme/app_color.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const Color primary = Color(0xFF105D38);
   static const Color secondary = Color(0xFFFFAE58);
+  static const Color black = Color(0xFF030319);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color white1 = Color(0xFFF6FAFD);
   static const Color green = Color(0xFF4CD080);
   static const Color gray = Color(0xFF8F92A1);
   static const Color muted = Color(0xFFBDBDBD);
