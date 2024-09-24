85a0f8489f343fa297885015fe3c373e706209a6
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-24 14:39:44 +0700

    feat: add color

---
+++ lib/utils/theme/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primary = Color(0xFF105D38);
+  static const Color secondary = Color(0xFFFFAE58);
+  static const Color green = Color(0xFF4CD080);
+  static const Color gray = Color(0xFF8F92A1);
+  static const Color muted = Color(0xFFBDBDBD);
+}
