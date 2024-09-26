c96410a808b78f3b6d806cc9fa1310481c331cd9
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-26 07:56:43 +0700

    feat: add news color

--- lib/utils/theme/app_color.dart
+++ lib/utils/theme/app_color.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const Color primary = Color(0xFF105D38);
   static const Color secondary = Color(0xFFFFAE58);
+  static const Color secondary1 = Color(0xFFFFD2A6);
   static const Color black = Color(0xFF030319);
   static const Color white = Color(0xFFFFFFFF);
   static const Color white1 = Color(0xFFF6FAFD);
   static const Color green = Color(0xFF4CD080);
+  static const Color green9 = Color(0xFF00373E);
   static const Color gray = Color(0xFF8F92A1);
   static const Color muted = Color(0xFFBDBDBD);
 }
