bfc30450349857f8bbff160de1eec4e7b8fb3ae4
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-26 07:15:27 +0700

    feat: add new assets svg

---
+++ lib/utils/constants/assets_svg.dart
@@ -0,0 +1,20 @@
+class AssetsSvg {
+  static const String _path = 'assets/icons/svg';
+
+  static const String logo = '$_path/logo.svg';
+  static const String asurance = '$_path/asurance.svg';
+  static const String bill = '$_path/bill.svg';
+  static const String electricity = '$_path/electricity.svg';
+  static const String history = '$_path/history.svg';
+  static const String internet = '$_path/internet.svg';
+  static const String merchant = '$_path/merchant.svg';
+  static const String mobileCredit = '$_path/mobile-credit.svg';
+  static const String more = '$_path/more.svg';
+  static const String setting = '$_path/setting.svg';
+  static const String topup = '$_path/top-up.svg';
+  static const String transfer = '$_path/transfer.svg';
+  static const String voucher = '$_path/voucher.svg';
+  static const String arrowBack = '$_path/arrow-back.svg';
+  static const String discount = '$_path/discount.svg';
+  static const String today = '$_path/today.svg';
+}
